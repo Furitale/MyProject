27203009ac67ce7c66f58182fcda4c8fa3a31d2c
Furitale <2208544961@qq.com>
2025-12-09 23:38:28 +0800

    初始提交：创建C语言项目基础结构

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "module1.h"
+
+int main() {
+    printf("Hello, Git实验项目!\n");
+    module1_function();
+    return 0;
+}
---
+++ src/module1.c
@@ -0,0 +1,6 @@
+#include "module1.h"
+#include <stdio.h>
+
+void module1_function() {
+    printf("这是模块1的功能\n");
+}
