b1f4fb301087a8cea0488dd5a829f1163929d145
Furitale <2208544961@qq.com>
2025-12-09 23:42:51 +0800

    feature: 增强module1模块功能，添加计算功能

--- src/module1.c
+++ src/module1.c
@@ -2,5 +2,8 @@
 #include <stdio.h>
 
 void module1_function() {
-    printf("这是模块1的功能\n");
+    printf("这是模块1的功能 - 增强版\n");
+    printf("新增功能：计算两个数的和\n");
+    int a = 5, b = 3;
+    printf("%d + %d = %d\n", a, b, a + b);
 }
